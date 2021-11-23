b3627404f111e581ab4cac71e420e1c053005892
Doston Khafizov <dhafizov99@gmail.com>
2021-11-23 14:18:35 -0500

    I made my landing page more interesting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
 <h1>Welcome to Doston Khafizov's Website!</h1>
 <hr>
 <img src="https://static01.nyt.com/images/2021/08/12/lens/12census-briefings-nyc1/merlin_189462357_a41cf263-7c3f-422f-88a7-38d9a7d4002a-superJumbo.jpg" alt="this is me laying on stairs">
+<hr>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/7bBvwIo0HzM?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</div>
+
+<style>
+    img{
+        width: 50%;
+    }
+    body{
+        background-color: #032424;
+        color: gold;
+        text-align: center;
+    }
+    .container{
+        width: 90%;
+        margin: auto;
+    }
+</style>
+
+
 </body>
 </html>
